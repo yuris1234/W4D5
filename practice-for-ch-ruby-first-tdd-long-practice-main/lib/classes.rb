b3dffa8bb9c028ca635178cd860aedc7ac7cce0b
yuris <yls4@cornell.edu>
2023-09-15 14:10:04 -0400

    working on two_sum

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/classes.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/classes.rb
@@ -11,15 +11,27 @@ class Array
 
     def two_sum
         holder = []
-
-        self.each_with_index do |ele1, i|
-            self.each_with_index do |ele2, j|
-                if j > i && ele1 + ele2 == 0
-                    holder << [i, j]
-                end
+        hash = Hash.new
+        self.each_with_index do |ele, i|
+            diff = -1 * ele 
+            key = hash[diff]
+            if key == nil 
+                hash[diff] = i 
+            else
+                holder << [hash[diff], i]
             end
         end
 
         holder
+
+        # self.each_with_index do |ele1, i|
+        #     self.each_with_index do |ele2, j|
+        #         if j > i && ele1 + ele2 == 0
+        #             holder << [i, j]
+        #         end
+        #     end
+        # end
+
+        # holder
     end
 end
