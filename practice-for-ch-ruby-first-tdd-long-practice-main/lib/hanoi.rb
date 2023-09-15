c77f5a5dfd29777006c86056d5a8d0f9137576f5
yuris <yls4@cornell.edu>
2023-09-15 15:45:02 -0400

    working on hanoi

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/hanoi.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/hanoi.rb
@@ -3,16 +3,71 @@ class Hanoi
 
     def initialize
         @pile1 = [4, 3, 2, 1]
-        @pile2 = []
-        @pile3 = []
+        @pile2 = ["_","_","_","_"]
+        @pile3 = ["_","_","_","_"]
         @towers = [@pile1, @pile2, @pile3]
     end
 
     def render
-        puts towers.join(" ")
+        towers.each do |pile|
+            print pile.join(" ") 
+            puts
+        end
     end
 
-    def move(pile_x, pile_y)
+    def move(pile_pop, pile_push)
+        pop = which_pile(pile_pop)
+        push = which_pile(pile_push)
+
+        raise if empty?(pop)
+
+        if peek(pop) < peek(push)
+            push << pop.pop 
+            return
+        else 
+            return 
+        end
         
     end
+
+    def which_pile(n)
+        raise if (1..3).include?(n)
+        case n
+        when 1 
+            return pile1 
+        when 2 
+            return pile2
+        when 3
+            return pile3 
+        end
+    end
+
+    def empty?(pile)
+        pile.all? {|ele| ele == "_"}
+    end
+
+    def peek(pile)
+        return 0 if pile.empty?
+        pile[-1]
+    end
+
+    def won?
+        pile3 == [4,3,2,1]
+    end
+
+    def play 
+        render
+        until won?
+            print "Enter a pile number to pop from, and then enter a pile number to push to, like this: 1 2."
+            puts
+            get = gets.chomp 
+            move(get.split(" ").map {|ele| ele.to_i})
+            render 
+        end
+        print "Victory! Game end."
+        puts
+    end
 end
+
+h = Hanoi.new 
+h.play
